35bbc8312d6af011ab68caadb1f98fcf3e2b1743
toefel <toefel18@gmail.com>
2018-11-18 18:57:53 +0100

    example of how routes could be set-up

--- garsson-api/src/main/kotlin/nl/toefel/garsson/main.kt
+++ garsson-api/src/main/kotlin/nl/toefel/garsson/main.kt
@@ -1,13 +1,8 @@
 package nl.toefel.garsson
 
-import io.undertow.Undertow
-import java.nio.charset.Charset
-
 fun main(args: Array<String>) {
-    val undertow = Undertow.builder()
-            .addHttpListener(8080, "0.0.0.0")
-            .setHandler { exchange -> exchange.responseSender.send("blaat", Charset.defaultCharset()) }
-            .build()
+    val server = Server(8080)
+    server.start()
 
-    undertow.start()
+    Runtime.getRuntime().addShutdownHook(Thread(Runnable { server.stop() }))
 }
