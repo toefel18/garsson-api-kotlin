883aec53f6d51e0acdf5369c25f843e8101923ad
Christophe Hesters <toefel18@gmail.com>
2019-06-08 23:30:20 +0200

    Ignoring BuildInfo

--- garsson-api/src/main/kotlin/nl/toefel/garsson/BuildInfo.kt
+++ garsson-api/src/main/kotlin/nl/toefel/garsson/BuildInfo.kt
@@ -6,9 +6,9 @@ package nl.toefel.garsson
 const val MAVEN_GROUP = ""
 const val MAVEN_NAME = "garsson-api"
 const val VERSION = "unspecified"
-const val GIT_REVISION = 47
-const val GIT_SHA = "004c66f155a2427560942b47bdab6f7f2a9f01f1"
-const val GIT_DATE = "2019-06-08T21:20:29Z"
-const val BUILD_DATE = "2019-06-08T21:23:05Z"
-const val BUILD_UNIX_TIME = 1560028985958L
+const val GIT_REVISION = 50
+const val GIT_SHA = "ce87fc255d2312f3a4de66e391338b3a2e3bad66"
+const val GIT_DATE = "2019-06-08T21:28:14Z"
+const val BUILD_DATE = "2019-06-08T21:28:17Z"
+const val BUILD_UNIX_TIME = 1560029297749L
 const val GIT_BRANCH = "master"
