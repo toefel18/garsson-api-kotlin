fcef389de72a38923d14e1582d5ef8aab9024787
toefel <toefel18@gmail.com>
2019-06-30 20:08:25 +0200

    Updated build info

--- garsson-api/src/main/kotlin/nl/toefel/garsson/BuildInfo.kt
+++ garsson-api/src/main/kotlin/nl/toefel/garsson/BuildInfo.kt
@@ -6,9 +6,9 @@ package nl.toefel.garsson
 const val MAVEN_GROUP = ""
 const val MAVEN_NAME = "garsson-api"
 const val VERSION = "unspecified"
-const val GIT_REVISION = 50
-const val GIT_SHA = "ce87fc255d2312f3a4de66e391338b3a2e3bad66"
-const val GIT_DATE = "2019-06-08T21:28:14Z"
-const val BUILD_DATE = "2019-06-08T21:28:17Z"
-const val BUILD_UNIX_TIME = 1560029297749L
+const val GIT_REVISION = 76
+const val GIT_SHA = "7ec2810b4f784000a086ca93e38f1fbd5d738655"
+const val GIT_DATE = "2019-06-30T18:06:21Z"
+const val BUILD_DATE = "2019-06-30T18:06:32Z"
+const val BUILD_UNIX_TIME = 1561917992427L
 const val GIT_BRANCH = "master"
